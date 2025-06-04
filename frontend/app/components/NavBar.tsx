98e8b6e7bd5801f0df95437c7b2208fa658a10bd
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-06-04 02:42:01 -0400

    favicon added

--- frontend/app/components/NavBar.tsx
+++ frontend/app/components/NavBar.tsx
@@ -1,5 +1,21 @@
-import { Box } from "@chakra-ui/react";
+import { 
+	Box,
+	Flex,
+	HStack
+} from "@chakra-ui/react";
+import Link from "next/link";
 
 export default function NavBar() {
-  return <Box></Box>;
+  return (
+    <Box
+			p="4"	
+		>
+			<Flex>
+				<HStack>
+					<p>stuff</p>
+					<p>stuff</p>
+				</HStack>
+			</Flex>
+    </Box>
+  );
 }
